fed43147d8b4d825ed1fb4df313af2627d612d25
amandabledsoe <amandajbledsoe@gmail.com>
2022-07-12 18:38:53 -0400

    initial MVC commit

---
+++ Unit7Lab1_CSR/Unit7Lab1_CSR/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Unit7Lab1_CSR.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
