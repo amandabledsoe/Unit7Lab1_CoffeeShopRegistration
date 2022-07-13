48fd546282f7eb80506111898c58ef2eb36b6e05
amandabledsoe <amandajbledsoe@gmail.com>
2022-07-12 21:49:09 -0400

    removing mvc commit

--- Unit7Lab1_CSR/Unit7Lab1_CSR/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Unit7Lab1_CSR.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
