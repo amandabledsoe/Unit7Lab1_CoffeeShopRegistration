ff2ad37d18c216cbe2d91ba96d1281a1fc62c8bd
amandabledsoe <amandajbledsoe@gmail.com>
2022-07-13 00:17:03 -0400

    MVC MVP! Correct dll issue where the filename was too long

---
+++ U7L1_CoffeeReg/U7L1_CoffeeReg/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using U7L1_CoffeeReg.Models;
+
+namespace U7L1_CoffeeReg.Controllers
+{
+    public class HomeController : Controller
+    {
+        CoffeeUser coffeeUser = new CoffeeUser();
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult CreateCoffeeUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateCoffeeUser(CoffeeUser newCoffeeUser)
+        {
+            if(ModelState.IsValid)
+            {
+                coffeeUser = newCoffeeUser;
+                return RedirectToAction("DisplayCoffeeUser", "Home", coffeeUser);
+            }
+            else
+            {
+                return View(newCoffeeUser);
+            }
+        }
+        [HttpPost]
+        public IActionResult DisplayCoffeeUser(string firstName, string lastName, string email, string password)
+        {
+            coffeeUser.FirstName = firstName;
+            coffeeUser.LastName = lastName;
+            coffeeUser.Email = email;
+            coffeeUser.Password = password;
+            return View(coffeeUser);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ U7L1_CoffeeReg/U7L1_CoffeeReg/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace U7L1_CoffeeReg.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
